3bea236ef49c4bd3494b42723c184dda435b2b06
coreight98 <maxcha98@naver.com>
2021-12-10 15:54:22 +0900

    fix : 오픈채팅방

--- Coreight98/programmers/오픈채팅방.js
+++ Coreight98/programmers/오픈채팅방.js
@@ -1,24 +1,14 @@
 function solution(record) {
   var answer = [];
-  const change = (command) => (user) => {
-    switch (command) {
-      case "Enter":
-        return `${user}님이 들어왔습니다.`;
-      case "Leave":
-        return `${user}님이 나갔습니다.`;
-    }
+  const change = {
+    Enter: (user) => `${user}님이 들어왔습니다.`,
+    Leave: (user) => `${user}님이 나갔습니다.`,
   };
   const list = record.reduce((result, element) => {
     const temp = element.split(" ");
     if (temp[2] !== undefined) result[temp[1]] = temp[2];
-    if (temp[0] !== "Change") {
-      answer.push({ command: temp[0], user: temp[1] });
-    }
+    if (temp[0] !== "Change") answer.push({ command: temp[0], user: temp[1] });
     return result;
   }, {});
-  answer = answer.map((element) => {
-    return change(element.command)(list[element.user]);
-  });
-
-  return answer;
+  return answer.map((element) => change[element.command](list[element.user]));
 }
