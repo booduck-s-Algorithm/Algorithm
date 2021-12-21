bbb92dd41920408bfa7d3f7091246815a15c6028
vgihan <jum654@naver.com>
2021-12-21 21:01:32 +0900

    programmers: 짝지어제거하기 (수정)
    - 시간 초과 문제 때문에 for 문 사용하는 로직으로 변경

--- vgihan/programmers/짝지어제거하기.js
+++ vgihan/programmers/짝지어제거하기.js
@@ -1,32 +1,11 @@
 function solution(str) {
-  const stack = new Stack();
-  str.split("").forEach((s) => {
-    if (stack.getTop() === s) {
+  const stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (stack[stack.length - 1] !== str[i]) {
+      stack.push(str[i]);
+    } else {
       stack.pop();
-      return;
     }
-    stack.push(s);
-  });
-  return stack.getJoinString() ? 0 : 1;
+  }
+  return stack.join("") ? 0 : 1;
 }
-function Stack() {
-  this.stack = [];
-}
-Stack.prototype.push = function (element) {
-  this.stack.push(element);
-};
-Stack.prototype.isEmpty = function () {
-  return this.stack.length === 0;
-};
-Stack.prototype.pop = function () {
-  if (this.isEmpty()) return;
-  const result = this.stack[this.stack.length - 1];
-  this.stack.splice(this.stack.length - 1, 1);
-  return result;
-};
-Stack.prototype.getTop = function () {
-  return this.stack[this.stack.length - 1];
-};
-Stack.prototype.getJoinString = function () {
-  return this.stack.join("");
-};
